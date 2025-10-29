2c27df5a197aae2228b18fdcd98b2be2cc4e7d46
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2025-10-28 18:56:05 -0700

    fetch connection

--- server/database_ops.py
+++ server/database_ops.py
@@ -11,5 +11,8 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 def fetch_user(name:str) -> User:
-    return User("", "", "", setup_dict = [x for x in json.loads(supabase.table("users")["data"]) if x["name"] == name][0])
+    return User( setup_dict = [x for x in json.loads(supabase.table("users")["data"]) if x["name"] == name][0])
+
+def fetch_connection(id:str) -> Connection:
+    return Connection(setup_dict=[x for x in json.loads(supabase.table("connections")["data"]) if x["id"] == id][0])
 
