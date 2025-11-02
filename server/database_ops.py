d5faab2f2413d3a2c9f81f6b7c10c7253165ecd2
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2025-11-01 17:49:51 -0700

    delete some duplicate functions

--- server/database_ops.py
+++ server/database_ops.py
@@ -48,15 +48,7 @@ def create_connection(
     return connection
 
 def fetch_message(id:str) -> Message:
-    return Message(setup_dict=[x for x in json.loads(supabase.table("messages")["data"]) if x["id"] == id][0])
-
-def create_user(name:str) -> User:
-    supabase.table("users").insert({"name":name}).execute()
-    return User(setup_dict=[x for x in json.loads(supabase.table("users")["data"]) if x["name"] == name][0])
-
-def create_connection(user1:User, user2:User) -> Connection:
-    supabase.table("connections").insert({"user1_id":user1.id, "user2_id":user2.id}).execute()
-    return Connection(setup_dict=[x for x in json.loads(supabase.table("connections")["data"]) if x["user1_id"] == user1.id and x["user2_id"] == user2.id][0])
+    return Message(setup_dict=[x for x in supabase.table("messages").execute()["data"] if x["id"] == id][0])
 
 def create_message(user:User, content:str) -> Message:
     supabase.table("messages").insert({"user_id":user.id, "content":content}).execute()
