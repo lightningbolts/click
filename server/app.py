891ca2f854ff5b3fb56699efda3dde7fda99c745
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2025-11-16 11:40:38 -0800

    new auth

--- server/app.py
+++ server/app.py
@@ -20,31 +20,10 @@ GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 
 
-def generate_refresh_token() -> str:
-    key = str(uuid.uuid4()).replace('-', '')[:32]
-    print(key)
-    keys = []
-    with open("refresh.json", "r") as f:
-        keys = json.loads(f.read())
-    keys.append(key)
-    with open("refresh.json", "w") as f:
-        f.write(json.dumps(keys))
-    return key
-
-def refresh_jwt_key(refresh: str) -> str:
-    with open("refresh.json", "r") as fp:
-        f = json.load(fp)
-        if(refresh in f):
-            encoded_jwt = jwt.encode({'cid': GOOGLE_CLIENT_ID, 'exp': time.time() + 86400},
-                                     GOOGLE_CLIENT_SECRET, algorithm="HS256")
-            return encoded_jwt
-        return "not allowed"
-
-
-def validate(encoded):
+def validate(encoded, email):
     try:
-        decoded = jwt.decode(encoded, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
-        if(decoded['org'] == "uw.edu" and decoded['exp'] >= time.time() and decoded['cid'] == GOOGLE_CLIENT_ID):
+        decoded = database_ops.supabase.auth.get_claims(encoded)
+        if(decoded["email"] == email and decoded['exp'] >= time.time() and decoded["aud"] == "authenticated"):
             return True
         else:
             return False
@@ -66,54 +45,9 @@ def create_account():
     else:
         return "log in!"
 
-@app.route('/google', methods=['POST'])
-def google():
-    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
-    try:
-        # Specify the WEB_CLIENT_ID of the app that accesses the backend:
-        idinfo = id_token.verify_oauth2_token(request.args['token'], requests.Request(), GOOGLE_CLIENT_ID)
-        if idinfo['exp'] < time.time():
-            return "expired token", 501
-        if idinfo['aud'] == GOOGLE_CLIENT_ID and 'accounts.google.com' in idinfo['iss'] and idinfo['exp'] >= time.time():
-            #plus one day
-            encoded_jwt = jwt.encode({'org': idinfo['hd'], 'cid': idinfo['aud'], 'exp': time.time() + 86400}, GOOGLE_CLIENT_SECRET, algorithm="HS256")
-            refreshToken = generate_refresh_token()
-            try_user =  database_ops.fetch_user_with_email(request.args["email"])
-            if try_user is not None:
-                return jsonify({"jwt": encoded_jwt, "refresh" : refreshToken, "user": try_user}), 200
-            return jsonify({"jwt": encoded_jwt, "refresh": refreshToken, "user": schema.User()}), 205
-        else:
-            return "not allowed", 403
-    except:
-        return "not allowed", 403
-
-@app.route("/refresh", methods=['POST'])
-def refresh():
-    res = refresh_jwt_key(request.headers['Authorization'])
-    if res == "not allowed":
-        return res, 403
-    return res, 200
-
-
-@app.route("/logout", methods=['POST'])
-def logout():
-    try:
-        refresh = request.headers['Authorization']
-        f = []
-        with open("refresh.json", "r") as fp:
-            f = json.load(fp)
-        if (refresh in f):
-            f.remove(refresh)
-        with open("refresh.json", "w") as fp:
-            json.dump(fp = fp, obj= f)
-        return "logged out"
-    except:
-        return "server error", 500
-
-
 @app.route("/user/<name>", methods=['GET'])
 def user(name):
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], request.args.get("email")):
         user_fetched = database_ops.fetch_user(name)
         if(user_fetched == None):
             return "user not found", 404
@@ -122,7 +56,7 @@ def user(name):
 
 @app.route("/user_with_email/<email>", methods=['GET'])
 def user_with_email(email):
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], email):
         user_fetched = database_ops.fetch_user_with_email(email)
         if(user_fetched == None):
             return "user not found", 404
@@ -131,14 +65,14 @@ def user_with_email(email):
 
 @app.route("/connections/", methods=['GET'])
 def connections():
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], request.args.get("email")):
         list = request.json["connections"]
         return jsonify([database_ops.fetch_connection(id) for id in list if not database_ops.delete_connection_if_expired(id)])
     return "log in", 405
 
 @app.route("/connection/<id>", methods=['GET'])
 def connection(id):
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], request.args.get("email")):
         if database_ops.delete_connection_if_expired(id):
             return "connection expired", 404
         return jsonify(database_ops.fetch_connection(id))
@@ -163,7 +97,7 @@ def new_connection():
 
 @app.route("/messages/restore", methods=['GET'])
 def message_restore():
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], request.args.get("email")):
         conn = fetch_connection(request.args.get("id"))
         return jsonify(conn.chat)
     return "log in", 405
@@ -171,7 +105,7 @@ def message_restore():
 #may need route for several messages
 @app.route("/message/new", methods=['POST'])
 def message_new():
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], request.args.get("email")):
         conn = fetch_connection(request.args.get("connid"))
         if not conn.has_begun:
             return 500
@@ -180,7 +114,7 @@ def message_new():
 
 @app.route("/pollpairs", methods=['POST'])
 def pollpairs():
-    if validate(request.headers['Authorization']):
+    if validate(request.headers['Authorization'], request.args.get("email")):
         user_id = request.args.get("id")
         user = database_ops.fetch_user_with_id(user_id)
         right_now = time.time()
@@ -218,13 +152,6 @@ def pollpairs():
                 return connection
             return 500
     return "log in", 405
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run()
