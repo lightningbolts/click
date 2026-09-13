e0060b5b302f3ee1836da73652300671f17778aa
Kairui Cheng <timberlake2025@gmail.com>
2026-09-13 15:46:03 -0700

    style: satisfy navigation icon lint

--- composeApp/src/commonMain/kotlin/compose/project/click/click/navigation/NavigationItem.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/navigation/NavigationItem.kt
@@ -1,7 +1,12 @@
 package compose.project.click.click.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class NavigationItem(
