cf6816e13fc5f21cdc9cd778a7dc4c11ff877da8
Kairui Cheng <timberlake2025@gmail.com>
2026-09-15 10:12:32 -0700

    fix: preserve map sheets beneath event hub overlay

---
+++ composeApp/src/androidMain/kotlin/compose/project/click/click/ui/components/PlatformNativeSheetOcclusion.android.kt
@@ -0,0 +1,8 @@
+@file:Suppress("ktlint:standard:function-naming")
+
+package compose.project.click.click.ui.components // pragma: allowlist secret
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun PlatformNativeSheetOcclusion(active: Boolean) = Unit
