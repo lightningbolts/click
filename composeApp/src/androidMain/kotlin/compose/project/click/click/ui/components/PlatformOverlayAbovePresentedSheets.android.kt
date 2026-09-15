f8d8c98212aa8273fd68971569ca8b616815b263
Kairui Cheng <timberlake2025@gmail.com>
2026-09-15 12:29:51 -0700

    feat: add Android overlay portal passthrough

---
+++ composeApp/src/androidMain/kotlin/compose/project/click/click/ui/components/PlatformOverlayAbovePresentedSheets.android.kt
@@ -0,0 +1,13 @@
+@file:Suppress("ktlint:standard:function-naming")
+
+package compose.project.click.click.ui.components // pragma: allowlist secret
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun PlatformOverlayAbovePresentedSheets(
+    liftAbovePresentedSheets: Boolean,
+    content: @Composable () -> Unit,
+) {
+    content()
+}
