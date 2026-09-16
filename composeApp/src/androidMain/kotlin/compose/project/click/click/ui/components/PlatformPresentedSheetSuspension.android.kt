4c13680748ea95a1d80ac8483daea86a8b6cd99b
Kairui Cheng <timberlake2025@gmail.com>
2026-09-16 10:58:25 -0700

    feat: add Android sheet suspension passthrough

---
+++ composeApp/src/androidMain/kotlin/compose/project/click/click/ui/components/PlatformPresentedSheetSuspension.android.kt
@@ -0,0 +1,8 @@
+@file:Suppress("ktlint:standard:function-naming")
+
+package compose.project.click.click.ui.components // pragma: allowlist secret
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun PlatformPresentedSheetSuspension(active: Boolean): Boolean = true
