694d0f55c87efb9b2b9de1f4978dcfc6af4a1961
Kairui Cheng <timberlake2025@gmail.com>
2026-09-16 11:24:03 -0700

    style: remove stale group avatar imports

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatMessageBubble.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatMessageBubble.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -45,7 +44,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
