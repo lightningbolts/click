d32413720918d9457a381ecc639de3216f06f47e
Kairui Cheng <timberlake2025@gmail.com>
2026-09-17 18:39:22 -0700

    style: remove unused icon button import

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ConnectionChatMessageComposer.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ConnectionChatMessageComposer.kt
@@ -30,7 +30,6 @@ import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.Mic
 import androidx.compose.material.icons.outlined.PhotoCamera
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
