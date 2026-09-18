b8d83c8198958b123089188c274f1ea8df53edf6
Kairui Cheng <timberlake2025@gmail.com>
2026-09-17 18:37:24 -0700

    style: remove unused composer import

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ConnectionChatMessageComposer.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ConnectionChatMessageComposer.kt
@@ -4,7 +4,6 @@ package compose.project.click.click.ui.chat
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
