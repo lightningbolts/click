6d8359ae51037c5231e785cfc8db8ba45cce267b
Kairui Cheng <timberlake2025@gmail.com>
2026-09-17 18:34:45 -0700

    feat: respect hub participant visibility for profile taps

--- composeApp/src/commonMain/kotlin/compose/project/click/click/viewmodel/HubChatViewModel.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/viewmodel/HubChatViewModel.kt
@@ -313,6 +313,9 @@ class HubChatViewModel(
 
     fun deleteMessage(messageId: String) = deleteHubMessageImpl(messageId)
 
+    fun canOpenSenderProfile(userId: String): Boolean =
+        userId.isNotBlank() && userId != currentUserId && userId in hubParticipantIds
+
     fun retryRealtime() {
         if (!startRealtime) return
         if (AppDataManager.isHubAccessRevoked(hubId)) {
