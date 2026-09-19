767e0bb9a829c52274eeb886656ca45c87e36310
Kairui Cheng <timberlake2025@gmail.com>
2026-09-18 18:00:59 -0700

    style: apply ktlint formatting to iOS barometer factory

--- composeApp/src/iosMain/kotlin/compose/project/click/click/sensors/IosBarometricHeightMonitor.kt
+++ composeApp/src/iosMain/kotlin/compose/project/click/click/sensors/IosBarometricHeightMonitor.kt
@@ -164,5 +164,4 @@ class IosBarometricHeightMonitor : BarometricHeightMonitor {
 }
 
 @Composable
-actual fun rememberBarometricHeightMonitor(): BarometricHeightMonitor =
-    remember { IosBarometricHeightMonitor() }
+actual fun rememberBarometricHeightMonitor(): BarometricHeightMonitor = remember { IosBarometricHeightMonitor() }
