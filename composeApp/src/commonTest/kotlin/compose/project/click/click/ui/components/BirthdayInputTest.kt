6680af0e6d89e9d377ab32eec3412ccdca4e95bf
Kairui Cheng <timberlake2025@gmail.com>
2026-09-18 17:03:49 -0700

    test: cover birthday visual formatting and offsets

--- composeApp/src/commonTest/kotlin/compose/project/click/click/ui/components/BirthdayInputTest.kt
+++ composeApp/src/commonTest/kotlin/compose/project/click/click/ui/components/BirthdayInputTest.kt
@@ -1,5 +1,6 @@
 package compose.project.click.click.ui.components
 
+import androidx.compose.ui.text.AnnotatedString
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -16,15 +17,19 @@ class BirthdayInputTest {
     }
 
     @Test
-    fun deletingAutoSeparatorRemovesOneAdjacentDigitInsteadOfReinsertingDash() {
-        assertEquals("200-04", editBirthdayDigitsInput("2000-04", "200004"))
-        assertEquals("2000-0", editBirthdayDigitsInput("2000-04-17", "2000-0417"))
+    fun editableStateContainsOnlyDigits() {
+        assertEquals("20000417", birthdayDigitsInput("2000-04-17"))
+        assertEquals("20000417", birthdayDigitsInput("2000/04/17"))
+        assertEquals("20000417", birthdayDigitsInput("2000-04-17T00:00:00Z"))
     }
 
     @Test
-    fun interactiveEditsStillNormalizePasteAndForwardTyping() {
-        assertEquals("2000-04-17", editBirthdayDigitsInput("", "2000/04/17"))
-        assertEquals("2000-04", editBirthdayDigitsInput("2000-0", "2000-04"))
+    fun visualTransformationMapsSeparatorsWithoutMakingThemEditable() {
+        val transformed = BirthdayVisualTransformation.filter(AnnotatedString("20000417"))
+        assertEquals("2000-04-17", transformed.text.text)
+        assertEquals(4, transformed.offsetMapping.transformedToOriginal(5))
+        assertEquals(6, transformed.offsetMapping.transformedToOriginal(8))
+        assertEquals(10, transformed.offsetMapping.originalToTransformed(8))
     }
 
     @Test
