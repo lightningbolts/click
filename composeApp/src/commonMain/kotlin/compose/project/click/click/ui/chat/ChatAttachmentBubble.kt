a11e8cc4d4246266d1b09aedc61433eed58c6862
Kairui Cheng <timberlake2025@gmail.com>
2026-09-20 08:50:50 -0700

    style(chat): keep Compose imports ordered

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatAttachmentBubble.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatAttachmentBubble.kt
@@ -1,8 +1,8 @@
 package compose.project.click.click.ui.chat
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
