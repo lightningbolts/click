e1dd408c4487d7522648855886cb12f85d2c3c51
Kairui Cheng <timberlake2025@gmail.com>
2026-09-20 19:34:57 -0700

    feat(chat): add draggable timeline scrollbar

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatMessageTimeline.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatMessageTimeline.kt
@@ -36,6 +36,7 @@ import compose.project.click.click.data.models.Connection
 import compose.project.click.click.data.models.Message
 import compose.project.click.click.data.models.MessageReaction
 import compose.project.click.click.data.models.MessageWithUser
+import compose.project.click.click.ui.components.DraggableLazyListScrollbar
 import compose.project.click.click.data.models.isBeaconChatMessage
 import compose.project.click.click.viewmodel.SecureChatMediaHost
 import kotlinx.coroutines.delay
@@ -352,5 +353,10 @@ internal fun ChatMessageTimeline(
                 }
             }
         }
+        DraggableLazyListScrollbar(
+            state = listState,
+            reverseLayout = true,
+            modifier = Modifier.align(Alignment.CenterEnd),
+        )
     }
 }
