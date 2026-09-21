b1d75f0edbfeeecc771379dd6a5dac04d1de75ee
Kairui Cheng <timberlake2025@gmail.com>
2026-09-20 19:44:00 -0700

    style: fix Kotlin import ordering

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatMessageTimeline.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/chat/ChatMessageTimeline.kt
@@ -36,8 +36,8 @@ import compose.project.click.click.data.models.Connection
 import compose.project.click.click.data.models.Message
 import compose.project.click.click.data.models.MessageReaction
 import compose.project.click.click.data.models.MessageWithUser
-import compose.project.click.click.ui.components.DraggableLazyListScrollbar
 import compose.project.click.click.data.models.isBeaconChatMessage
+import compose.project.click.click.ui.components.DraggableLazyListScrollbar
 import compose.project.click.click.viewmodel.SecureChatMediaHost
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
