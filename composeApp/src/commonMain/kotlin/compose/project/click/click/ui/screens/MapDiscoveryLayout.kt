63ef69545ddee5ac11c3e68213e2959adcde5d0c
Kairui Cheng <timberlake2025@gmail.com>
2026-09-20 19:44:05 -0700

    style: fix Kotlin import ordering

--- composeApp/src/commonMain/kotlin/compose/project/click/click/ui/screens/MapDiscoveryLayout.kt
+++ composeApp/src/commonMain/kotlin/compose/project/click/click/ui/screens/MapDiscoveryLayout.kt
@@ -94,8 +94,8 @@ import compose.project.click.click.ui.components.ClickSearchField // pragma: all
 import compose.project.click.click.ui.components.ClickSheetDefaults // pragma: allowlist secret
 import compose.project.click.click.ui.components.ConnectionListUserAvatarFace // pragma: allowlist secret
 import compose.project.click.click.ui.components.DiscoverySortSegmentBar // pragma: allowlist secret
-import compose.project.click.click.ui.components.GlassSheetTokens // pragma: allowlist secret
 import compose.project.click.click.ui.components.DraggableLazyListScrollbar // pragma: allowlist secret
+import compose.project.click.click.ui.components.GlassSheetTokens // pragma: allowlist secret
 import compose.project.click.click.ui.components.LocalSheetOnDismissRequest // pragma: allowlist secret
 import compose.project.click.click.ui.components.ProvideSheetSwipeDismiss // pragma: allowlist secret
 import compose.project.click.click.ui.components.rememberCardVisual // pragma: allowlist secret
